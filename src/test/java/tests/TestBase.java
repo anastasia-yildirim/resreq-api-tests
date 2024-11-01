33b283c2b1dc390ed0642c4546b2669afe19a372
anastasiayildirim <anastasia.skrydlova@gmail.com>
2024-11-01 21:35:48 +0700

    add allure steps to after each for debug

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,12 +1,14 @@
 package tests;
 
 import config.TestEnvironmentConfigurator;
+import io.qameta.allure.Allure;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static helpers.Attachments.generateDataForAllureReport;
+import static helpers.Attachments.*;
 import static helpers.LoginExtension.clearSession;
+import static io.qameta.allure.Allure.step;
 
 public class TestBase {
 
@@ -19,8 +21,22 @@ public class TestBase {
 
     @AfterEach
     void shutDown() {
-        generateDataForAllureReport();
-        closeWebDriver();
-        clearSession();
+        //generateDataForAllureReport();
+
+//        screenshotAs("Last screenshot");
+//        pageSource();
+//        browserConsoleLogs();
+//        addVideo();
+//
+//        closeWebDriver();
+//        clearSession();
+
+        step("Take last screenshot", () -> screenshotAs("Last screenshot"));
+        step("Capture page source", () -> pageSource());
+        step("Log browser console messages", () -> browserConsoleLogs());
+        step("Add video recording to report", () -> addVideo());
+
+        step("Close the WebDriver", () -> closeWebDriver());
+        step("Clear browser session", () -> clearSession());
     }
 }
